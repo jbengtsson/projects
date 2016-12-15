36dd40d1e5b1ca43a517e3755a30008c93347164
jbengtsson <jbngtssn@gmail.com>
2016-12-15 12:10:54 +0100

    Code clean-up.

--- src/dnu.cc
+++ src/dnu.cc
@@ -7,24 +7,17 @@ int  no_tps = NO;
 
 int main(int argc, char *argv[])
 {
-
-  const long  seed = 1121;
-
   globval.H_exact    = false; globval.quad_fringe = false;
   globval.Cavity_on  = false; globval.radiation   = false;
   globval.emittance  = false; globval.IBS         = false;
   globval.pathlength = false; globval.bpm         = 0;
 
-  // disable from TPSALib- and LieLib log messages
-//  idprset(-1);
-
   if (false)
     Read_Lattice(argv[1]);
   else
-    rdmfile("flat_file.dat");
+    rdmfile(argv[1]);
 
   globval.EPU = false;
 
   dnu_dA(4e-3, 4e-3, 0.0, 25); get_ksi2(3.0e-2);
-
 }
