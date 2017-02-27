a29f8cba31dcf3cb2ae7dff423d4df7037d20833
jbengtsson <jbngtssn@gmail.com>
2017-02-27 15:14:18 +0100

    Code tweak.

--- src/max_iv_2.cc
+++ src/max_iv_2.cc
@@ -5,13 +5,15 @@
 int no_tps = NO;
 
 
-const double eps_x   = 15e-3,
-             nu_uc[] = {4.0/15.0, 3.0/15.0}, // Cell tune.
-             L_uc    = 1.25,                 // Unit Cell length.
-             L_ss    = 14.0,                 // Unit Cell length.
-             eta1_x  = 0.00357357,           // Optics at center of Bh.
-             beta0[] = {1.59916, 0.57961},   // Optics at center of SFh.
-             beta1[] = {3.0,     3.0};       // Optics at center of straight.
+const double eps_x       = 15e-3,
+             nu_uc[]     = {4.0/15.0, 3.0/15.0}, // Cell tune.
+             L_uc        = 1.2,                  // Unit Cell length.
+             L_ss        = 14.0,                 // Unit Cell length.
+             eta_cuc[]   = {0.00357357, 0.0},    // Center of unit cell.
+             etap_cuc[]  = {0.0, 0.0},           // Center of unit cell.
+             alpha_cuc[] = {0.0, 0.0},           // Center of unit cell.
+             beta_cuc[]  = {1.59916, 0.57961},
+             beta_cs[]   = {3.0, 3.0};           // Center of straight.
 
 const string sf_sd_name[] = {"sfh", "sd"};
 
@@ -218,7 +220,7 @@ void prt_emit(const param_type &b2_prms, const double *b2)
 
 double f_emit(double *b2)
 {
-  // Minimize linear linear chromaticity for unit cell.
+  // Optimize unit cell.
   // Lattice: unit cell.
 
   static double chi2_ref = 1e30;
@@ -226,7 +228,7 @@ double f_emit(double *b2)
   int    i, sf_sd[2];
   double eps1_x, tr[2], b3L[2], a3L, chi2;
 
-  const double scl_eps = 1e2, scl_L = 1e1, scl_ksi = 1e-5;
+  const double scl_eps = 1e2, scl_L = 1e0, scl_ksi = 1e-5;
 
   b2_prms.set_prm(b2);
 
@@ -285,7 +287,7 @@ double f_emit(double *b2)
 
 void fit_emit(param_type &b2_prms)
 {
-  // Minimize linear linear chromaticity for unit cell.
+  // Optimize unit cell.
   // Lattice: unit cell.
 
   int    n_b2, i, j, iter;
@@ -320,7 +322,8 @@ void prt_match(const param_type &b2_prms, const double *b2)
 
   fprintf(outf, "l5h: drift, l = %7.5f;\n", get_L(ElemIndex("l5h"), 1));
   fprintf(outf, "l6:  drift, l = %7.5f;\n", get_L(ElemIndex("l6"), 1));
-  fprintf(outf, "l7h: drift, l = %7.5f;\n", get_L(ElemIndex("l7h"), 1));
+  // fprintf(outf, "l7h: drift, l = %7.5f;\n", get_L(ElemIndex("l7h"), 1));
+  fprintf(outf, "l7:  drift, l = %7.5f;\n", get_L(ElemIndex("l7"), 1));
   fprintf(outf, "l8:  drift, l = %7.5f;\n", get_L(ElemIndex("l8"), 1));
 
   fprintf(outf, "\nbm:  bending, l = 0.14559, t = 0.5, k = %9.5f, t1 = 0.0"
@@ -336,9 +339,100 @@ void prt_match(const param_type &b2_prms, const double *b2)
 }
 
 
+double f_hcell(double *b2)
+{
+  // Match half super period.
+  // Lattice: half super period.
+
+  static double chi2_ref = 1e30;
+
+  int          i, loc1, loc2;
+  double       tr[2], chi2;
+  ss_vect<tps> A;
+
+  const double scl_eta = 1e4, scl_alpha = 1e0, scl_beta = 1e0, scl_L = 0e1;
+
+  b2_prms.set_prm(b2);
+
+  // End of bm.
+  loc1 = Elem_GetPos(ElemIndex("bm"), 1);
+  // Center of straight.
+  loc2 = globval.Cell_nLoc;
+
+  A = get_A(alpha_cuc, beta_cuc, eta_cuc, etap_cuc);
+  Cell_Twiss(0, globval.Cell_nLoc, A, false, false, 0e0);
+
+  chi2 = 0e0;
+  chi2 += sqr(scl_eta*Cell[loc1].Eta[X_]);
+  chi2 += sqr(scl_eta*Cell[loc1].Etap[X_]);
+  chi2 += sqr(scl_alpha*Cell[loc2].Alpha[X_]);
+  chi2 += sqr(scl_alpha*Cell[loc2].Alpha[Y_]);
+  chi2 += sqr(scl_beta*(Cell[loc2].Beta[X_]-beta_cs[X_]));
+  chi2 += sqr(scl_beta*(Cell[loc2].Beta[Y_]-beta_cs[Y_]));
+  chi2 += sqr(scl_L*(Cell[globval.Cell_nLoc].S-L_ss));
+
+  if ((fabs(tr[X_]) > 2e0) || (fabs(tr[Y_]) > 2e0)) chi2 += 1e10;
+  for (i = 1; i <= b2_prms.n_prm; i++) {
+    if ((b2_prms.n[i-1] == -1) && (b2[i] < b2_prms.bn_min[i-1]))
+      chi2 += 1e10;
+    if (fabs(b2[i]) > b2_prms.bn_max[i-1]) chi2 += 1e10;
+  }
+
+  if (chi2 < chi2_ref) {
+    printf("\nchi2: %12.5e, %12.5e\n", chi2, chi2_ref);
+    printf("b: %10.3e %10.3e %10.3e %10.3e %10.5f %10.5f %10.5f\n",
+	   Cell[loc1].Eta[X_],  Cell[loc1].Etap[X_],
+	   Cell[loc2].Alpha[X_], Cell[loc2].Alpha[Y_],
+	   Cell[loc2].Beta[X_],  Cell[loc2].Beta[Y_],
+	   Cell[globval.Cell_nLoc].S);
+    printf("b2s: ");
+    for (i = 1; i <= b2_prms.n_prm; i++)
+      printf(" %9.5f", b2[i]);
+    printf("\n");
+
+    prtmfile("flat_file.fit");
+    prt_match(b2_prms, b2);
+
+    prt_lat("linlat1.out", globval.bpm, true);
+    prt_lat("linlat.out", globval.bpm, true, 10);
+  }
+
+  chi2_ref = min(chi2, chi2_ref);
+
+  return chi2;
+}
+
+
+void fit_hcell(param_type &b2_prms)
+{
+  // Minimize Optimize super period.
+  // Lattice: super period.
+
+  int    n_b2, i, j, iter;
+  double *b2, *b2_lim, **xi, fret;
+
+  n_b2 = b2_prms.n_prm;
+
+  b2 = dvector(1, n_b2); b2_lim = dvector(1, n_b2);
+  xi = dmatrix(1, n_b2, 1, n_b2);
+
+  b2_prms.ini_prm(b2, b2_lim);
+
+  // Set initial directions (unit vectors).
+  for (i = 1; i <= n_b2; i++)
+    for (j = 1; j <= n_b2; j++)
+      xi[i][j] = (i == j)? 1e0 : 0e0;
+
+  dpowell(b2, xi, n_b2, 1e-16, &iter, &fret, f_hcell);
+
+  free_dvector(b2, 1, n_b2);  free_dvector(b2_lim, 1, n_b2);
+  free_dmatrix(xi, 1, n_b2, 1, n_b2);
+}
+
+
 double f_match(double *b2)
 {
-  // Minimize linear linear chromaticity for super period.
+  // Optimize super period.
   // Lattice: super period.
 
   static double chi2_ref = 1e30;
@@ -346,7 +440,8 @@ double f_match(double *b2)
   int          i, loc1, loc2, loc3, loc4;
   double       tr[2], chi2;
 
-  const double scl_eta = 1e3, scl_beta = 1e0, scl_L = 0e1, scl_ksi = 1e-2;
+  const double scl_eta = 1e3, scl_alpha = 1e2, scl_beta = 1e0,
+               scl_L   = 0e1, scl_ksi   = 0e-2;
 
   b2_prms.set_prm(b2);
 
@@ -366,17 +461,19 @@ double f_match(double *b2)
   // printf("trace: %6.3f %6.3f\n", tr[X_], tr[Y_]);
 
   chi2 = 0e0;
-  chi2 += sqr(scl_eta*(Cell[loc1].Eta[X_]-eta1_x));
-  chi2 += sqr(scl_eta*Cell[loc1].Etap[X_]);
-  chi2 += sqr(Cell[loc2].Beta[X_]-beta0[X_]);
-  chi2 += sqr(Cell[loc2].Beta[Y_]-beta0[Y_]);
-  chi2 += sqr(scl_eta*Cell[loc3].Eta[X_]);
-  chi2 += sqr(scl_eta*Cell[loc3].Etap[X_]);
-  chi2 += sqr(scl_beta*Cell[loc4].Beta[X_]-beta1[X_]);
-  chi2 += sqr(scl_beta*Cell[loc4].Beta[Y_]-beta1[Y_]);
-  chi2 += sqr(scl_L*(Cell[globval.Cell_nLoc].S-L_ss));
-  chi2 += sqr(scl_ksi*globval.Chrom[X_]);
-  chi2 += sqr(scl_ksi*globval.Chrom[Y_]);
+  // chi2 += sqr(scl_eta*(Cell[loc1].Eta[X_]-eta_bh_x));
+  // chi2 += sqr(scl_eta*Cell[loc1].Etap[X_]);
+  // chi2 += sqr(scl_alpha*(Cell[loc1].Alpha[X_]-alpha_bh[X_]));
+  // chi2 += sqr(scl_alpha*(Cell[loc1].Alpha[Y_]-alpha_bh[Y_]));
+  // chi2 += sqr(scl_beta*(Cell[loc2].Beta[X_]-beta_sfh[X_]));
+  // chi2 += sqr(scl_beta*(Cell[loc2].Beta[Y_]-beta_sfh[Y_]));
+  // chi2 += sqr(scl_eta*Cell[loc3].Eta[X_]);
+  // chi2 += sqr(scl_eta*Cell[loc3].Etap[X_]);
+  // chi2 += sqr(scl_beta*(Cell[loc4].Beta[X_]-beta_cs[X_]));
+  // chi2 += sqr(scl_beta*(Cell[loc4].Beta[Y_]-beta_cs[Y_]));
+  // chi2 += sqr(scl_L*(Cell[globval.Cell_nLoc].S-L_ss));
+  // chi2 += sqr(scl_ksi*globval.Chrom[X_]);
+  // chi2 += sqr(scl_ksi*globval.Chrom[Y_]);
 
   if ((fabs(tr[X_]) > 2e0) || (fabs(tr[Y_]) > 2e0)) chi2 += 1e10;
   for (i = 1; i <= b2_prms.n_prm; i++) {
@@ -387,12 +484,13 @@ double f_match(double *b2)
 
   if (chi2 < chi2_ref) {
     printf("\nchi2: %12.5e, %12.5e\n", chi2, chi2_ref);
-    printf("b:    %10.3e %10.3e %10.5f %10.5f %10.3e %10.3e %10.5f %10.5f"
-	   " %10.5f %10.5f %10.5f\n",
-	   Cell[loc1].Etap[X_], Cell[loc1].Etap[X_],
-	   Cell[loc2].Beta[X_], Cell[loc2].Beta[Y_],
-	   Cell[loc3].Eta[X_], Cell[loc3].Etap[X_],
-	   Cell[loc4].Beta[X_], Cell[loc4].Beta[Y_],
+    printf("b: %10.3e %10.3e %10.3e %10.3e %10.5f %10.5f  %10.3e %10.3e"
+	   "   %10.5f %10.5f\n %10.5f %10.5f %10.5f\n",
+	   Cell[loc1].Etap[X_],  Cell[loc1].Etap[X_],
+	   Cell[loc1].Alpha[X_], Cell[loc1].Alpha[Y_],
+	   Cell[loc2].Beta[X_],  Cell[loc2].Beta[Y_],
+	   Cell[loc3].Eta[X_],   Cell[loc3].Etap[X_],
+	   Cell[loc4].Beta[X_],  Cell[loc4].Beta[Y_],
 	   Cell[globval.Cell_nLoc].S,
 	   globval.Chrom[X_], globval.Chrom[Y_]);
     printf("b2s: ");
@@ -414,7 +512,7 @@ double f_match(double *b2)
 
 void fit_match(param_type &b2_prms)
 {
-  // Minimize linear linear chromaticity for super period.
+  // Minimize Optimize super period.
   // Lattice: super period.
 
   int    n_b2, i, j, iter;
@@ -468,7 +566,7 @@ int main(int argc, char *argv[])
     b2_prms.add_prm("l2", -1, 0.05, 0.35, 0.01);
     b2_prms.add_prm("bh", -1, 0.05, 0.3,  0.01);
 
-    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-8; b2_prms.step = 0.001;
+    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-8; b2_prms.step = 1.0;
 
     no_sxt();
     fit_emit(b2_prms);
@@ -482,14 +580,31 @@ int main(int argc, char *argv[])
 
     b2_prms.add_prm("l5h", -1, 0.05, 1.0,  0.01);
     b2_prms.add_prm("l6",  -1, 0.05, 1.0,  0.01);
-    b2_prms.add_prm("l7h", -1, 0.05, 1.0,  0.01);
-    b2_prms.add_prm("l8",  -1, 0.05, 1.0,  0.01);
+    b2_prms.add_prm("l7",  -1, 0.05, 1.0,  0.01);
+
+    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-8; b2_prms.step = 1.0;
+
+    no_sxt();
+    fit_hcell(b2_prms);
+  }
+
+  if (false) {
+    b2_prms.add_prm("bm",   2, 0.0, 25.0, 1.0);
+    b2_prms.add_prm("qfe",  2, 0.0, 25.0, 1.0);
+    b2_prms.add_prm("qde",  2, 0.0, 25.0, 1.0);
+    b2_prms.add_prm("qm",   2, 0.0, 25.0, 1.0);
+
+    b2_prms.add_prm("l5h", -1, 0.05, 1.0,  0.01);
+    b2_prms.add_prm("l6",  -1, 0.05, 1.0,  0.01);
+    b2_prms.add_prm("l7",  -1, 0.05, 1.0,  0.01);
+    // b2_prms.add_prm("l7h", -1, 0.05, 1.0,  0.01);
+    // b2_prms.add_prm("l8",  -1, 0.05, 1.0,  0.01);
 
     // b2_prms.add_prm("bm",  -2,  0.05, 0.5, 1.0);
     // b2_prms.add_prm("qde", -2,  0.05, 0.5, 1.0);
     // b2_prms.add_prm("qm",  -2,  0.05, 0.5, 1.0);
 
-    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-8; b2_prms.step = 0.001;
+    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-8; b2_prms.step = 1.0;
 
     no_sxt();
     fit_match(b2_prms);
